14b85910740c3b4e8b485e5037f46f3eec8ea18a
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-09-27 16:51:47 -0500

    GiftExpertApp.js: Añadiendo una serie con el boton

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,15 +1,23 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const GiftExpertApp = () => {
 
-    const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
+    const [categories, setcategories] = useState( ['One Punch', 'Samurai X', 'Dragon Ball'] );
+
+    const handleAdd = () => {
+        setcategories([...categories,'HunterXHunter']);
+    }
 
     return (
         <>
             <h2>Gift Expert App</h2>
             <hr/>
+            
+            <button onClick={ handleAdd }>Agregar</button>
 
             <ol>
                 {
