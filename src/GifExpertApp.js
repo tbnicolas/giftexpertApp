9528721f2fbcb4cb532f7c8dce3f8310edfcd1ef
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-09-27 16:46:29 -0500

    GifExperteApp.js: Añadiendo una lista al componente

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,10 +3,21 @@ import React from 'react';
 
 
 const GiftExpertApp = () => {
+
+    const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+
     return (
         <>
             <h2>Gift Expert App</h2>
             <hr/>
+
+            <ol>
+                {
+                    categories.map( ( category, index ) =>  {
+                        return <li key={ category }> { category } </li>
+                    })
+                }
+            </ol>
         </>
     );
 }
