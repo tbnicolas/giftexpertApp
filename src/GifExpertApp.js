8314444b0e334a74adc25e7e01d4a92ca39e79ed
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2021-09-27 20:21:32 -0500

    GifExpertApp.js: Remplazando por un componente hijo 'GifGrid'

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,13 +1,14 @@
 
 import React, { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 
 const GiftExpertApp = () => {
 
     //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
 
-    const [categories, setcategories] = useState( ['One Punch', 'Samurai X', 'Dragon Ball'] );
+    const [categories, setcategories] = useState( ['Super Campeones'] );
 
     /* const handleAdd = () => {
         //setcategories([...categories,'HunterXHunter']);
@@ -24,7 +25,10 @@ const GiftExpertApp = () => {
             <ol>
                 {
                     categories.map( ( category, index ) =>  {
-                        return <li key={ category }> { category } </li>
+                        return <GifGrid
+                                 key={category}
+                                 category={category} 
+                               />
                     })
                 }
             </ol>
